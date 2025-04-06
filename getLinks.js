f2e9226986012aa67dd90016b1ec81401a11938a
Ahmad Huthaifa <ahmadtalk@gmail.com>
2025-04-06 06:30:04 +0300

    Update getLinks.js

--- getLinks.js
+++ getLinks.js
@@ -1,9 +1,5 @@
 // getLinks.js
 
-const supabaseUrl = 'https://wbkvftjmtnihcavfwikg.supabase.co'; // ضع رابط مشروعك من Supabase
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6India3ZmdGptdG5paGNhdmZ3aWtnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM5MDIyMTIsImV4cCI6MjA1OTQ3ODIxMn0.8ExDJ3Y4nzufeDMuRl82VWPK_3LI14qj5-6qitiCJq0'; // ضع هنا مفتاح anon key الخاص بك
-const supabase = supabase.createClient(supabaseUrl, supabaseKey);
-
 async function fetchLinks() {
     try {
         document.getElementById('loading').classList.remove('d-none');
