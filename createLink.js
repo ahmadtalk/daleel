76aa0d4c86ce73324c2ae04be00a8cd93b37357e
Ahmad Huthaifa <ahmadtalk@gmail.com>
2025-04-06 06:30:19 +0300

    Update createLink.js

--- createLink.js
+++ createLink.js
@@ -1,9 +1,5 @@
 // createLink.js
 
-const supabaseUrl = 'https://wbkvftjmtnihcavfwikg.supabase.co'; // ضع هنا رابط مشروعك
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6India3ZmdGptdG5paGNhdmZ3aWtnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM5MDIyMTIsImV4cCI6MjA1OTQ3ODIxMn0.8ExDJ3Y4nzufeDMuRl82VWPK_3LI14qj5-6qitiCJq0'; // ضع هنا مفتاح anon key الخاص بك
-const supabase = supabase.createClient(supabaseUrl, supabaseKey);
-
 const linkForm = document.getElementById('linkForm');
 
 linkForm.addEventListener('submit', async (e) => {
