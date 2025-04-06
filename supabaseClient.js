114bb0211c953ec7a313d856c46ac8e9218d4b6e
Ahmad Huthaifa <ahmadtalk@gmail.com>
2025-04-06 07:59:22 +0300

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -2,4 +2,4 @@
 
 const supabaseUrl = 'https://wbkvftjmtnihcavfwikg.supabase.co'; // رابط مشروعك
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6India3ZmdGptdG5paGNhdmZ3aWtnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM5MDIyMTIsImV4cCI6MjA1OTQ3ODIxMn0.8ExDJ3Y4nzufeDMuRl82VWPK_3LI14qj5-6qitiCJq0'; // مفتاح anon key
-const supabase = supabase.createClient(supabaseUrl, supabaseKey);
+window.supabase = window.supabase || supabase.createClient(supabaseUrl, supabaseKey);
