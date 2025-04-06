6e5250f627d43a13de07e3ea63e88a935472bf2d
Ahmad Huthaifa <ahmadtalk@gmail.com>
2025-04-06 06:27:12 +0300

    Create supabaseClient.js

---
+++ supabaseClient.js
@@ -0,0 +1,5 @@
+// supabaseClient.js
+
+const supabaseUrl = 'https://YOUR_PROJECT_REF.supabase.co'; // رابط مشروعك
+const supabaseKey = 'YOUR_ANON_KEY'; // مفتاح anon key
+const supabase = supabase.createClient(supabaseUrl, supabaseKey);
