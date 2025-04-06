600308c50eb549429e8b5e4a70f58e77a4d011fc
Ahmad Huthaifa <ahmadtalk@gmail.com>
2025-04-06 06:30:33 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
 // تهيئة Supabase
-const supabaseUrl = 'https://wbkvftjmtnihcavfwikg.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6India3ZmdGptdG5paGNhdmZ3aWtnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM5MDIyMTIsImV4cCI6MjA1OTQ3ODIxMn0.8ExDJ3Y4nzufeDMuRl82VWPK_3LI14qj5-6qitiCJq0';
-const supabase = createClient(supabaseUrl, supabaseKey);
 
 // جلب الروابط
 async function fetchLinks() {
